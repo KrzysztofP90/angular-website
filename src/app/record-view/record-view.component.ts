13aea0f709ae6c03dbf16f7e05ef42a9c7c201b4
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-05-29 13:37:35 +0200

    Implement example content of record-view-component depends from route path.

--- src/app/record-view/record-view.component.ts
+++ src/app/record-view/record-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RecordFromDB } from '../model/record';
+import { LoadingDataService} from '../service/loading-data.service';
 
 @Component({
   selector: 'app-record-view',
@@ -9,10 +11,12 @@ import { Router } from '@angular/router';
 export class RecordViewComponent implements OnInit {
 
   private path: string;
+  private recordData: RecordFromDB;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private dataService: LoadingDataService ) {}
 
   ngOnInit() {
     this.path = this.router.url;
+    this.recordData = this.dataService.getFakeRecordByPath(this.path);
   }
 }
