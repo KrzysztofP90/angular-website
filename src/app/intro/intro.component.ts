a5ce74e18e5fb9bb5da8effd356cc18f78863437
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-05-30 18:53:16 +0200

    Add new example images and add new example contact and gallery records in intro component.

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -11,9 +11,13 @@ import { LoadingDataService } from '../service/loading-data.service';
 export class IntroComponent implements OnInit {
 
   private records: Array<RecordFromDB>;
+  private gallery: RecordFromDB;
+  private contact: RecordFromDB;
   
   constructor(private dataService: LoadingDataService) { 
     this.records = this.dataService.getFakeRecordsArray();
+    this.contact = this.dataService.getContact();
+    this.gallery = this.dataService.getGallery();
   }
 
   ngOnInit() {
