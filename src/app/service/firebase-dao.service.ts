0213c893fb6cde92258151adaf602b5ce0200c28
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-05-31 20:55:08 +0200

    Implement Fire base DAO service.

--- src/app/service/firebase-dao.service.ts
+++ src/app/service/firebase-dao.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { FirebaseRecord } from '../model/firebase-record';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FirebaseDaoService {
 
+  private records: Observable<FirebaseRecord[]>;
 
   constructor(private dao: AngularFirestore) {
+      this.records = this.dao.collection('records').valueChanges();
+  }
 
+  getRecords() {
+    return this.records;
   }
 }
