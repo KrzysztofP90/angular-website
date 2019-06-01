f4041fe2b779dab31c25eeb93b9d5b17717e9515
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-06-01 20:57:53 +0200

    Implement providing contact and gallery data from fire base in  prepare data helper.

--- src/app/service/prepare-data-helper.service.ts
+++ src/app/service/prepare-data-helper.service.ts
@@ -12,7 +12,7 @@ export class PrepareDataHelperService {
   // temporary data for image
   arrayOfMiniImagePath: Array<string> = [
     'assets/builder.jpg', 'assets/builder3.jpg', 'assets/builder7.jpg',
-    'assets/cat-4151445_960_720.jpg'
+    'assets/minicontact.jpg', 'assets/cat-4151445_960_720.jpg'
   ];
   // temporary data for image
   arrayOfImagePath: Array<string> = [
@@ -27,11 +27,22 @@ export class PrepareDataHelperService {
   createUsefulRecordsArrayFromFirebaseRecordsArray(fbRecords: FirebaseRecord[]) {
     const arrayOfRecords = new Array<RecordFromDB>();
     let id = 0;
+    const galleryOrContactId = 99;
     for (const rec of fbRecords) {
-      arrayOfRecords.push(new RecordFromDB(rec.title, rec.description,
-        rec.path + id.toString(), rec.buttonLabel, rec.mainContent, this.arrayOfMiniImagePath[id],
-        this.arrayOfImagePath[id], id));
-      id++;
+      if (rec.path !== 'gallery' && rec.path !== 'contact') {
+        arrayOfRecords.push(new RecordFromDB(rec.title, rec.description,
+          rec.path + id.toString(), rec.buttonLabel, rec.mainContent,
+          this.arrayOfMiniImagePath[id], this.arrayOfImagePath[id], id));
+        id++;
+      }
+    }
+    for (const rec of fbRecords) {
+      if (rec.path === 'gallery' || rec.path === 'contact') {
+        arrayOfRecords.push(new RecordFromDB(rec.title,
+          rec.description, rec.path, rec.buttonLabel, rec.mainContent,
+           this.arrayOfMiniImagePath[id], this.arrayOfImagePath[id], galleryOrContactId));
+        id++;
+      }
     }
     this.records = arrayOfRecords;
   }
