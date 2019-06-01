c9e0367fa4581d276e1ed60d71405bfc6f306fd7
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-06-01 14:17:45 +0200

    Implement alfa version of reading record views text contexts from fire base - problem with ajax to solve.

--- src/app/record-view/record-view.component.ts
+++ src/app/record-view/record-view.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { RecordFromDB } from '../model/record';
 import { LoadingFakeDataService } from '../service/loading-data.service';
+import { PrepareDataHelperService } from '../service/prepare-data-helper.service';
+import { FirebaseDaoService } from '../service/firebase-dao.service';
+import { FirebaseRecord } from '../model/firebase-record';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -13,12 +17,19 @@ export class RecordViewComponent implements OnInit {
 
   private records: Array<RecordFromDB>;
   private currentId: number;
+  private recordsObserv: Observable<FirebaseRecord[]>;
 
   constructor(private router: Router, private dataService: LoadingFakeDataService, 
-    private activatedRoute: ActivatedRoute ) {}
+    private activatedRoute: ActivatedRoute, private dao: FirebaseDaoService,
+    private helper: PrepareDataHelperService ) {}
 
   ngOnInit() {
-    this.records = this.dataService.getFakeRecordsArray();
+    // this.records = this.dataService.getFakeRecordsArray();
+    
+    this.recordsObserv = this.dao.getRecordsObservable();  /// think about separate it to service
+    this.recordsObserv.subscribe(records => {
+      this.records = this.helper.convertFirebaseRecordsArrayToUsefulRecordsArray(records);
+    });
     this.activatedRoute.params.subscribe(routeParams => {
       this.setCurrentId(routeParams.id);
     });
