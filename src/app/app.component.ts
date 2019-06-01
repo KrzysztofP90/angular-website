612af1946e3e530fdec8ce9a8e9e40bb1a5a4836
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-06-01 12:45:34 +0200

    Implement read menu options with paths from fire base. Remove fake records.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RecordFromDB } from '../app/model/record';
 import { LoadingDataService } from '../app/service/loading-data.service';
 import { FirebaseDaoService } from './service/firebase-dao.service';
+import { FirebaseRecord } from './model/firebase-record';
+import { Observable } from 'rxjs';
+import { PrepareDataHelperService } from './service/prepare-data-helper.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [LoadingDataService,
-    FirebaseDaoService]
+    FirebaseDaoService,
+    PrepareDataHelperService]
 
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
 
   private records: Array<RecordFromDB>;
-  
-  constructor(private dataService: LoadingDataService, private dao: FirebaseDaoService) {
-    /// generate fake example records not from DB
-    this.records = this.dataService.getFakeRecordsArray();
-  }
+  private recordsObserv: Observable<FirebaseRecord[]>;
 
-  getRecords() {
-    return this.records;
+  constructor(private dao: FirebaseDaoService, private helper: PrepareDataHelperService) {}
+
+  ngOnInit() {
+    this.recordsObserv = this.dao.getRecordsObservable();
+    this.recordsObserv.subscribe(records => {
+      this.records = this.helper.convertFirebaseRecordsArrayToUsefulRecordsArray(records); });
   }
 
   onActivate(event) {
