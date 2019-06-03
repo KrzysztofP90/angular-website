d7f1a0846383a0138b8789c7125efb763043c0e5
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-06-03 18:41:47 +0200

    Implement gallery images service.

--- src/app/service/gallery.service.ts
+++ src/app/service/gallery.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GalleryService {
 
-  constructor() { }
+  constructor(private dao: AngularFirestore ) {
+    this.loadImagesFromFirebase();
+   }
+
+  private imagesFromGallery: Observable<any>;
+
+  loadImagesFromFirebase() {
+    this.imagesFromGallery = this.dao.collection('gallery-img').valueChanges();
+  }
+
+  getGalleryImagesFromGallery() {
+    return this.imagesFromGallery;
+  }
 }
