f21dbd2f0ffc82de57f983048c1ae0958438702d
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-06-09 14:13:19 +0200

    Implement log out.

--- src/app/cms/cms.component.ts
+++ src/app/cms/cms.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseDaoService } from '../service/firebase-dao.service';
 import { PrepareDataHelperService } from '../service/prepare-data-helper.service';
 import { FirebaseRecord } from '../model/firebase-record';
+import { AuthService } from '../service/auth.service';
 
 
 @Component({
@@ -13,14 +14,18 @@ export class CmsComponent implements OnInit {
 
   private records: Array<FirebaseRecord>;
 
-  constructor(public dao: FirebaseDaoService, public helper: PrepareDataHelperService) { }
+  constructor(public dao: FirebaseDaoService, public helper: PrepareDataHelperService,
+    private auth: AuthService ) { }
 
   ngOnInit() {
     this.dao.getRecordsObservable().subscribe( rec => {
     this.helper.sortFireBaseArray(rec);
     this.records = rec;
     });
+  }
 
+  logOut() {
+    this.auth.logout();
   }
 
 }
