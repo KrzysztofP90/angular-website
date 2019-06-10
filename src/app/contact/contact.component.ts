7d5cb12435fead09509a21635a9236015c8589d3
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-06-10 23:26:37 +0200

    Implement get contact  data from user in form.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,6 +14,25 @@ export class ContactComponent implements OnInit {
   private contact: FirebaseRecord;
   private records: FirebaseRecord[];
 
+  private email: any = {
+    email: '',
+    message: ''
+  };
+
+  private quest: string;
+
+  private JsonToSend: any = {
+      senderEmail: "",
+      emailPassword: "",
+      consumerEmail: "",
+      smtpPort: 587,
+      portForSocket: 587,
+      host: 'smtp.gmail.com',
+      messageText: ""
+  };
+
+
+
   constructor(public dao: FirebaseDaoService, public helper: PrepareDataHelperService) {}
 
   ngOnInit() {
@@ -26,6 +45,24 @@ export class ContactComponent implements OnInit {
 
   getContactRecordId(arr: FirebaseRecord[]) {
     return this.records.length - 1;
+  }
+
+  createMessageToClientFromMessage() {
+    const message = this.email.message + '\n Your message from client - please answer to '
+    + this.email.email;
+    this.email.message = message;
+  }
+
+  createJSONtoSend() {
+    this.JsonToSend.message = this.email.message;
+    /// temporary
+    this.JsonToSend.consumerEmail = 'krzysztof1.przybylowicz@gmail.com';
+  }
+
+  sendEmail() {
+    this.createMessageToClientFromMessage();
+    this.createJSONtoSend();
+    console.log(this.JsonToSend);
   }
 
 }
